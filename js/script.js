80924175246ccb40e37af6741c919e6009f0f532
Vladimir@TEVLA <vladimir@tevla.co.uk>
2016-09-30 17:37:55 +0100

    added xml loading and parsing

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 	var container;
 	var space;
 
-	var _sol;
+	var _data;
 
 	window.onload = init;
 
@@ -14,12 +14,23 @@
 		container = document.getElementById('container');
 		space = document.getElementById('space');
 
-//		_sol = new StellarObject('Sol');
-//		_sol.init();
-
 		setCanvas100Percent();
+        
+        //load data xml file and convert to json
+        var objson = xml2json.fromFile('js/stellarobjects/data/SolarSystem.xml');
+        console.log(objson.system.star.satellites.stellarobject.name["#text"]);
+        console.log(objson.system.star.satellites.stellarobject.name["@attributes"].shorthand);
+        console.log(objson.system.star.satellites.stellarobject.name["@attributes"].unit);
 	}
 
+    
+    function handleJSONload(xml) {
+        console.log("solar system loaded");
+        var xmlDoc = xml.responseXML;
+        console.log(xmlDoc)
+        
+    }    
+    
 
 	function setCanvas100Percent(){
 		space.width = container.offsetWidth;
