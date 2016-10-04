e0d62848d9406c0432168f35b600b24b7bdfaaac
Vladimir@TEVLA <vladimir@tevla.co.uk>
2016-10-04 15:52:39 +0100

    figured out the ratio to create different planet or object of different sizes

--- js/utils/Perspective.js
+++ js/utils/Perspective.js
@@ -3,9 +3,10 @@ var RELATIVE    = "relative";
 var FALSESCALE    = "falseScale";
 
 var RELATIVE_SCALE = 0.0005;
-var FALSE_SCALE = [100,50,25,12];
 
-var size=RELATIVE_SCALE;
+var FALSE_SCALE = [1, 3, 6,12,25,50,100];
+
+var size=FALSESCALE;
 
 
 
@@ -23,5 +24,31 @@ function relativeSize(value){
 }
 
 function falseScaleSize(value){
-    return 100;
+    //earth is set to 30 pixels, everything else is a ratio based on the equatorial radius
+    
+    var ratio = value / 6378;
+    
+    //anything smaller than half of earth will be a moon
+    
+    if (ratio<0.3)
+        return FALSE_SCALE[0];
+    
+    if (ratio>=0.1 && ratio<0.3)
+        return FALSE_SCALE[1];
+    
+    if (ratio>=0.3 && ratio<0.5)
+        return FALSE_SCALE[2];
+    
+    if (ratio>=0.5 && ratio<1)
+        return FALSE_SCALE[3];
+    
+    if (ratio>=1 && ratio<3)
+        return FALSE_SCALE[4];
+
+    if (ratio>=3 && ratio<10)
+        return FALSE_SCALE[5];    
+
+    if (ratio>=10)
+        return FALSE_SCALE[6];
+
 }
